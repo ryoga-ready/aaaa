ad739b98bf41e8e74aa12c13a30f43ff7388ccf7
ryoga-ready <accelerator_11_318@yahoo.co.jp>
2026-04-03 01:08:26 +0900

    Initial commit

---
+++ Music Mood.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+   const splash=document.getElementById("splash");
+   splash.classList.add("fadeout")
+   
+   setTimeout(()=>{
+    splash.style.display="none";
+    document.getElementById("menu").classList.remove("hidden");
+    menu.classList.add("show");
+   },1000);
+}, 2000);
