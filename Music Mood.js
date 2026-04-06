0c0e09cd50a0bd90ba1bc82126fbf7620415c900
ryoga-ready <accelerator_11_318@yahoo.co.jp>
2026-04-06 13:22:45 +0900

    気分で音楽に出会う

--- Music Mood.js
+++ Music Mood.js
@@ -4,9 +4,63 @@ setTimeout(() => {
    
    setTimeout(()=>{
     splash.style.display="none";
-    document.getElementById("menu").classList.remove("hidden");
-    menu.classList.add("show");
+    document.getElementById("menu1")
+    const menu1=document.getElementById("menu1");
+    menu1.classList.add("show");
    },1000);
 }, 2000);
 
-/*test*/
+
+
+function searchScene(question1){
+   const menucontainer1=document.getElementById("menu-container1")
+   const menu1=document.getElementById("menu1");
+   const read1=document.getElementById("read1")
+   const answer1=document.querySelector(".answer1")
+            answer1.textContent=question1;
+
+   menucontainer1.classList.add("fadeout")
+   setTimeout(()=>{
+       menucontainer1.style.display = "none";
+       menucontainer1.classList.remove("fadeout");
+       menucontainer1.classList.remove("show");
+     setTimeout(()=>{
+      read1.style.display = "block";
+      read1.classList.remove("fadeout");
+      read1.classList.remove("show");
+      setTimeout(()=>{
+      read1.classList.add("show");
+      },10);
+   },10);
+   },1000);
+   
+
+setTimeout(()=>{
+ read1.classList.add("fadeout")
+setTimeout(()=>{
+read1.style.display = "none";
+read1.classList.remove("fadeout");
+   },1000);
+},3000);
+   
+
+
+
+   const btn21=document.querySelector(".btn1")
+   const btn22=document.querySelector(".btn2")
+   const btn23=document.querySelector(".btn3")
+   const btn24=document.querySelector(".btn4")
+   const Q2=document.querySelector(".Q1")
+   const S2=document.querySelector(".S1")
+
+   setTimeout(()=>{
+       menucontainer1.style.display = "flex";
+       read1.classList.remove("show");
+       setTimeout(()=>{
+       menucontainer1.classList.add("fadein");
+      },10);
+},4000);
+
+
+
+}
